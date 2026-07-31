16067f80aea10a512c3d13dbed425f236431d1ce
TilekarPranav <pranavtilekar8034@gmail.com>
2026-07-31 12:42:50 +0530

    Fix GitHub Actions Kafka configuration

--- src/main/java/com/ecommerce/config/KafkaProducerConfig.java
+++ src/main/java/com/ecommerce/config/KafkaProducerConfig.java
@@ -8,15 +8,16 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
+@Profile("!test")
 public class KafkaProducerConfig {
-
-	@Value("${spring.kafka.bootstrap-servers}")
+	@Value("${spring.kafka.bootstrap-servers:localhost:9092}")
 	private String bootstrapServers;
 
 	@Bean
