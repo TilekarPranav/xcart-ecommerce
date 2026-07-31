ab73579026da130dfa35171154b8c61d5ba8c927
TilekarPranav <pranavtilekar8034@gmail.com>
2026-07-31 10:39:12 +0530

    test

--- src/test/java/com/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/ecommerce/ECommerceApplicationTests.java
@@ -2,13 +2,18 @@ package com.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ECommerceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@MockitoBean
+	private KafkaTemplate<String, Object> kafkaTemplate;
+
+	@Test
+	void contextLoads() {
+	}
 }
